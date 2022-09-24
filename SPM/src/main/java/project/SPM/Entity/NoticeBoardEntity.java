c2722c6154811fb25a24cf554c055959b05bc477
ohju96 <ohju96@naver.com>
2022-09-24 16:38:46 +0900

    :sparkles: [#18] feature : Dto와 Entity 정의

---
+++ SPM/src/main/java/project/SPM/Entity/NoticeBoardEntity.java
@@ -0,0 +1,54 @@
+package project.SPM.Entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "NOTICE")
+@DynamicInsert //Eitntiy 변수 값이 null인 변수 제외하고 insert 쿼리 생성
+@DynamicUpdate // 데이터 수정할 때 null 제외 - 게시판 내용 중 제목만 수정할 때 제목만 변경하는 update 쿼리 생성
+@Builder
+@Entity
+public class NoticeBoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long noticeBoardSeq;
+
+    @NonNull
+    @Column(name = "title", length = 500, nullable = false)
+    private String title;
+
+    @NonNull
+    @Column(name = "notice_yn", length = 1, nullable = false)
+    private String noticeYn;
+
+    @NonNull
+    @Column(name = "contents", nullable = false)
+    private String contents;
+
+    @NonNull
+    @Column(name = "contents", nullable = false)
+    private String userId;
+
+    @Column(name = "read_cnt", nullable = false)
+    private Long readCnt;
+
+    @Column(name = "reg_id", updatable = false)
+    private String regId;
+
+    @Column(name = "reg_dt", updatable = false)
+    private String regDt;
+
+    @Column(name = "chg_id")
+    private String chgId;
+
+    @Column(name = "chg_dt")
+    private String chgDt;
+}
